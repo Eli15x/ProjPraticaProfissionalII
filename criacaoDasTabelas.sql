8fa5cac09cab72cec9d50a9b86291c06d69ecd68
EmmanuelCasangel <30930846+EmmanuelCasangel@users.noreply.github.com>
2018-03-27 09:02:15 -0300

    terminei de criar as tabelas

--- criacaoDasTabelas.sql
+++ criacaoDasTabelas.sql
@@ -5,10 +5,35 @@ create table Time(
 
  create table Usuario(
  codUsuario int primary key identity,
- nomeUsuario varchar(20) not null,
+ nomeUsuario varchar(30) not null,
  senha varchar(30) not null,
  maiorPontuacao int null,
  codTime int not null,
  constraint fkCodTime foreign key(codTIme) References Time(codTime)
  )
+
+
+create table Materia(
+codMateria int primary key identity,
+nomeMateria varchar(20) not null
+)
+
+create table Pergunta(
+codPergunta int primary key identity,
+pergunta varchar(200) not null,
+dificultade int not null,
+codMateria int not null,
+constraint fkCodMateria foreign key(codMateria) References Materia(codMateria)
+) 
+
+
+create table Resposta(
+codResposta int primary key identity,
+resposta varchar(40) not null,
+status bit not null,
+codPergunta int not null,
+constraint fkCodPErgunta foreign key(codPergunta) References Pergunta(codPergunta)
+) 
+
+
 
