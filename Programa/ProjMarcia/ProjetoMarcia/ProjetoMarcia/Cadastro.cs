086dd52caf260fc8f2364dccc5ea9d8a6e05b33e
EmmanuelCasangel <30930846+EmmanuelCasangel@users.noreply.github.com>
2018-04-10 10:48:11 -0300

    arrumando git
    fd

--- Programa/ProjMarcia/ProjetoMarcia/ProjetoMarcia/Cadastro.cs
+++ Programa/ProjMarcia/ProjetoMarcia/ProjetoMarcia/Cadastro.cs
@@ -13,10 +13,8 @@ namespace ProjetoMarcia
 {
     public partial class frmCadastro : Form
     {
-<<<<<<< HEAD
-=======
 
->>>>>>> a2ecaf69c4592e99d6271fca6bb143ad8a67dfe1
+   
         string cs = Properties.Settings.Default.BDPRII17171ConnectionString;
 
         public frmCadastro()
@@ -35,11 +33,10 @@ namespace ProjetoMarcia
 
             if ((txtUsuCad.Text == "") || (txtSenCad.Text == "")||(cmbTimeCad.Text == ""))
                 MessageBox.Show("Preencher todos os campos");
-<<<<<<< HEAD
-            if ((cmbTimeCad.Text == "VErmelho") || (cmbTimeCad.Text=="Azul"))
+
+            if (!(cmbTimeCad.Text == "Vermelho") || (cmbTimeCad.Text=="Azul"))
                 MessageBox.Show("Cor de time inválida");
-=======
->>>>>>> a2ecaf69c4592e99d6271fca6bb143ad8a67dfe1
+
 
             else
             {
@@ -55,43 +52,21 @@ namespace ProjetoMarcia
 
 
                     // cria comando de consulta ao SQL 
-<<<<<<< HEAD
-                    string cmd_s = "Insert into Usuario values(@usu, @sen,0,@time)";
-=======
-                    string cmd_s = "insert into Usuario values(@usu,@sen,0,@time)";
->>>>>>> a2ecaf69c4592e99d6271fca6bb143ad8a67dfe1
 
+                    string cmd_s = "Insert into Usuario values(@usu, @sen,0,@time)";
                     SqlCommand cmd = new SqlCommand(cmd_s, con);
                     cmd.Parameters.AddWithValue("@usu", txtUsuCad.Text);
                     cmd.Parameters.AddWithValue("@sen", txtSenCad.Text);
-<<<<<<< HEAD
+
                     if (cmbTimeCad.Text == "Azul")
                         cmd.Parameters.AddWithValue("@time", 4);
                     else
                         cmd.Parameters.AddWithValue("@time", 1);
-
-=======
-                    if(cmbTimeCad.Text=="Azul")
-                        cmd.Parameters.AddWithValue("@time", 4);
-                    else
-                        if(cmbTimeCad.Text == "Vermelho")
-                            cmd.Parameters.AddWithValue("@time", 1);
-                        else
-                            MessageBox.Show("Valor Invalido para time");
->>>>>>> a2ecaf69c4592e99d6271fca6bb143ad8a67dfe1
-
+                    
                     con.Open();
-
                     cmd.ExecuteNonQuery();
-
                     con.Close();
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> a2ecaf69c4592e99d6271fca6bb143ad8a67dfe1
+                    
                 }
                 catch (System.Data.SqlClient.SqlException ex)
                 {
@@ -101,11 +76,7 @@ namespace ProjetoMarcia
                     MessageBox.Show(str, "Database Exception");
                 }
             }
-<<<<<<< HEAD
-            
-=======
 
->>>>>>> a2ecaf69c4592e99d6271fca6bb143ad8a67dfe1
         }
 
 
