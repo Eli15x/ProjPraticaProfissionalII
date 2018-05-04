2c7fcde9c28e6885b239df51bc265080c0980d67
EmmanuelCasangel <30930846+EmmanuelCasangel@users.noreply.github.com>
2018-05-04 09:35:17 -0300

    inseridas no banco

---
+++ questoes de matematica.sql
@@ -0,0 +1,256 @@
+ 
+
+insert into Materia values('Matematica')
+
+select * from Materia
+
+
+delete from Resposta where codPergunta=11
+
+insert into Pergunta values('Quanto vale em segundos 4mim e 57s',1,1)
+
+insert into Resposta values ('297s',1,1)
+insert into Resposta values ('307s',0,1)
+insert into Resposta values ('197s',0,1)
+insert into Resposta values ('397s',0,1)
+
+
+insert into Pergunta values('Quanto vale em minutos 8h e 37m',1,1)
+
+insert into Resposta values ('497min',0,2)
+insert into Resposta values ('537min',0,2)
+insert into Resposta values ('517min',1,2)
+insert into Resposta values ('627min',0,2)
+
+
+insert into Pergunta values('Quanto vale em metros 7km',1,1)
+
+insert into Resposta values ('700m' ,0,3)
+insert into Resposta values ('7000m'  ,1,3)
+insert into Resposta values ('70000m',0,3)
+insert into Resposta values ('7700m' ,0,3)
+
+
+insert into Pergunta values('Calcule: 304 * 2',1,1)
+
+insert into Resposta values ('616',0,4)
+insert into Resposta values ('604',0,4)
+insert into Resposta values ('612',0,4)
+insert into Resposta values ('608',1,4)
+
+
+insert into Pergunta values('Calcule: 37 * 10',1,1)
+
+insert into Resposta values ('370',1,5)
+insert into Resposta values ('360',0,5)
+insert into Resposta values ('380',0,5)
+insert into Resposta values ('390',0,5)
+
+
+insert into Pergunta values('Calcule: 9 * 2',1,1)
+
+insert into Resposta values ('28',0,6)
+insert into Resposta values ('17',0,6)
+insert into Resposta values ('18',1,6)
+insert into Resposta values ('21',0,6)
+
+
+insert into Pergunta values('Calcule: 7 * 3',1,1)
+
+insert into Resposta values ('21',1,7)
+insert into Resposta values ('18',0,7)
+insert into Resposta values ('24',0,7)
+insert into Resposta values ('23',0,7)
+
+insert into Pergunta values('Calcule: 36/6',1,1)
+
+insert into Resposta values ('8',0,8)
+insert into Resposta values ('7',0,8)
+insert into Resposta values ('6',1,8)
+insert into Resposta values ('5',0,8)
+
+
+insert into Pergunta values('Calcule: 81/9',1,1)
+
+insert into Resposta values ('7' ,0,9)
+insert into Resposta values ('10',0,9)
+insert into Resposta values ('8' ,0,9)
+insert into Resposta values ('9' ,1,9)
+
+
+insert into Pergunta values('Calcule: 36/9',1,1)
+
+insert into Resposta values ('4',1,10)
+insert into Resposta values ('5',0,10)
+insert into Resposta values ('3',0,10)
+insert into Resposta values ('6',0,10)
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+
+insert into Pergunta values('Simplifique: 21/6',2,1)
+
+insert into Resposta values ('7/2',1,11)
+insert into Resposta values ('8/3',0,11)
+insert into Resposta values ('7/3',0,11)
+insert into Resposta values ('8/5',0,11)
+
+
+insert into Pergunta values('Simplifique: 16/10',2,1)
+
+insert into Resposta values ('7/4',0,12)
+insert into Resposta values ('4/3',0,12)
+insert into Resposta values ('8/3',0,12)
+insert into Resposta values ('8/5',1,12)
+
+
+insert into Pergunta values('Simplifique: 18/12',2,1)
+
+insert into Resposta values ('3/2',1,13)
+insert into Resposta values ('4/3',0,13)
+insert into Resposta values ('5/3',0,13)
+insert into Resposta values ('2/3',0,13)
+
+insert into Pergunta values('Simplifique: 20/15',2,1)
+
+insert into Resposta values ('5/2',0,14)
+insert into Resposta values ('4/3',1,14)
+insert into Resposta values ('5/3',0,14)
+insert into Resposta values ('2/3',0,14)
+
+
+insert into Pergunta values('Calcule: 99/2',2,1)
+
+insert into Resposta values ('44.5',0,15)
+insert into Resposta values ('49.5',1,15)
+insert into Resposta values ('48.5',0,15)
+insert into Resposta values ('43.5',0,15)
+
+
+insert into Pergunta values('Calcule: 13/5',2,1)
+
+insert into Resposta values ('2.5',0,16)
+insert into Resposta values ('2.8',0,16)
+insert into Resposta values ('2.6',1,16)
+insert into Resposta values ('2.7',0,16)
+
+
+insert into Pergunta values('Calcule: 30/5',2,1)
+
+insert into Resposta values ('6',1,17)
+insert into Resposta values ('8',0,17)
+insert into Resposta values ('4',0,17)
+insert into Resposta values ('7',0,17)
+
+
+insert into Pergunta values('Calcule: 528 * 10',2,1)
+
+insert into Resposta values ('5290',0,18)
+insert into Resposta values ('5280',1,18)
+insert into Resposta values ('5380',0,18)
+insert into Resposta values ('5270',0,18)
+
+
+insert into Pergunta values('Calcule: 30 * 5',2,1)
+
+insert into Resposta values ('180',0,19)
+insert into Resposta values ('160',0,19)
+insert into Resposta values ('150',1,19)
+insert into Resposta values ('170',0,19)
+
+
+insert into Pergunta values('Calcule: 229 + 245',2,1)
+
+insert into Resposta values ('473',0,20)
+insert into Resposta values ('471',0,20)
+insert into Resposta values ('472',0,20)
+insert into Resposta values ('474',1,20)
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+insert into Pergunta values('Calcule: 455 * 5',3,1)
+
+insert into Resposta values ('2335',0,21)
+insert into Resposta values ('2255',0,21)
+insert into Resposta values ('2375',0,21)
+insert into Resposta values ('2275',1,21)
+
+insert into Pergunta values('Calcule: 441 * 5',3,1)
+
+insert into Resposta values ('2205',1,22)
+insert into Resposta values ('2105',0,22)
+insert into Resposta values ('2405',0,22)
+insert into Resposta values ('2305',0,22)
+
+
+insert into Pergunta values('Calcule:346 * 8 ',3,1)
+
+insert into Resposta values ('2128',0,23)
+insert into Resposta values ('2788',0,23)
+insert into Resposta values ('2778',0,23)
+insert into Resposta values ('2768',1,23)
+
+
+insert into Pergunta values('Carlos tinha 13 caixas com 24 cadernos cada uma. Ele perdeu 10 cadernos. Quantos cadernos ele tem agora',3,1)
+
+insert into Resposta values ('404',0,24)
+insert into Resposta values ('302',1,24)
+insert into Resposta values ('312',0,24)
+insert into Resposta values ('292',0,24)
+--eu apertei f5 duas veze na pergunta, dai tive que excluir(e era identity...)
+--delete from Pergunta where codPergunta = 25
+
+
+insert into Pergunta values('Paulo tinha 14 caixas com 18 lápis cada uma. Quantos lápis ele tem',3,1)
+
+insert into Resposta values ('248',0,26)
+insert into Resposta values ('258',0,26)
+insert into Resposta values ('252',1,26)
+insert into Resposta values ('292',0,26)
+
+
+insert into Pergunta values('Paulo tinha 18 caixas com 10 lápis cada uma, ele perdeu 6 lápis.Quantos lápis ele tem agora',3,1)
+
+insert into Resposta values ('166',0,27)
+insert into Resposta values ('184',0,27)
+insert into Resposta values ('176',0,27)
+insert into Resposta values ('174',1,27)
+
+
+insert into Pergunta values('A fração 7/3 corresponde a:',3,1)
+
+insert into Resposta values ('3.5',1,28)
+insert into Resposta values ('4.5',0,28)
+insert into Resposta values ('3.6',0,28)
+insert into Resposta values ('3.2',0,28)
+
+
+insert into Pergunta values('A fração 8/5 corresponde a:',3,1)
+
+insert into Resposta values ('1.5',0,29)
+insert into Resposta values ('2.5',0,29)
+insert into Resposta values ('1.6',1,29)
+insert into Resposta values ('1.8',0,29)
+
+select * from Pergunta
+select * from Resposta
+insert into Pergunta values('A fração 6/15 corresponde a:',3,1)
+
+insert into Resposta values ('3.1',0,30)
+insert into Resposta values ('2.5',1,30)
+insert into Resposta values ('2.6',1,30)
+insert into Resposta values ('1.8',0,30)
+
+insert into Pergunta values('A fração 70/9 corresponde a:',3,1)
+
+insert into Resposta values ('7.777...',1,31)
+insert into Resposta values ('7.666...',0,31)
+insert into Resposta values ('6.777...',1,31)
+insert into Resposta values ('7.888...',0,31)
+
+
+
